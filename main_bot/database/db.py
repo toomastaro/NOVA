ebdb25fdf672b7089c457271b6983ed8456e16d4
Toomas Taro <toomas.taro@gmail.com>
2025-12-02 09:03:00 +0200

    feat: Добавлен централизованный класс Database для агрегации CRUD операций и создания таблиц.

--- main_bot/database/db.py
+++ main_bot/database/db.py
@@ -16,6 +16,7 @@ from main_bot.database.published_post.crud import PublishedPostCrud
 from main_bot.database.story.crud import StoryCrud
 from main_bot.database.exchange_rate.crud import ExchangeRateCrud
 from main_bot.database.novastat.crud import NovaStatCrud
+from main_bot.database import engine, Base
 
 
 class Database(
