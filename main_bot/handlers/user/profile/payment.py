e583177cea21057587cb0a839b89a5752312a108
Toomas Taro <toomas.taro@gmail.com>
2025-12-05 15:41:07 +0200

    feat: Добавлена функциональность обработки платежей для пополнения баланса, включая поддержку Crypto Bot, Platega, Telegram Stars и промокодов.

--- main_bot/handlers/user/profile/payment.py
+++ main_bot/handlers/user/profile/payment.py
@@ -280,6 +280,7 @@ def hand_add():
     router = Router()
     router.callback_query.register(choice, F.data.split("|")[0] == "ChoicePaymentMethod")
     router.callback_query.register(cancel, F.data.split("|")[0] == "BalanceAmountCancel")
+    router.callback_query.register(back_to_method, F.data.split("|")[0] == "WaitBalancePaymentBack")
     router.message.register(get_promo, Balance.input_promo, F.text)
     router.message.register(get_amount, Balance.input_amount, F.text)
     router.pre_checkout_query.register(process_pre_checkout_query)
