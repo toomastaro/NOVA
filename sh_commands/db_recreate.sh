04e98b633063f426ab9d34b0bfb60528e3d6539a
Toomas Taro <toomas.taro@gmail.com>
2025-12-17 15:57:16 +0200

    11

--- sh_commands/db_recreate.sh
+++ sh_commands/db_recreate.sh
@@ -1,10 +1,13 @@
 # Остановить контейнер
 docker compose down
 
-# Удалить volume с данными (очистка под 0)
+# Удалить volume с данными PostgreSQL (очистка под 0)
 docker volume rm имя_volume
 # или если в compose используется volume postgres_data:
 docker volume rm $(docker volume ls -q | grep postgres_data)
+
+# Удалить volume с данными Redis (полная очистка кэша)
+docker volume rm $(docker volume ls -q | grep redis_data)
 
 # Перезапустить всё заново
 docker compose up -d
