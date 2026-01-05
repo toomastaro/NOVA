3d736b6dc188cd254c69a297186544bb2f5def01
Toomas Taro <toomas.taro@gmail.com>
2026-01-05 07:46:17 +0000

    CI: bump version to 1.0.871

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config:
     и другие параметры, загружаемые из переменных окружения.
     """
 
-    VERSION = "1.0.870"
+    VERSION = "1.0.871"
 
     # Настройки бота
     BOT_TOKEN = os.getenv("BOT_TOKEN")
